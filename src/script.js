5e39f100c10f0d405180ea34bce1642addd68c23
Nick Cianfarra <nicholascian99@gmail.com>
2023-12-21 16:02:02 -0500

    Progress on search
    search now correctly displays all movies that fit the search and their data
    
    bug: displays videogames/tv shows as well instead of just movies. Information is usually "N/A" in these cases

--- src/script.js
+++ src/script.js
@@ -1,48 +1,85 @@
+let searched = ""
+
+// const searchBar = document.getElementById("search-bar")
 
 
-  
+//adds click event to submit button that fetches OMDB API and uses that data when it calls renderMovieHTML
+
 document.getElementById("search-bar-submit").addEventListener('click', function(){
-    fetch("https://www.omdbapi.com/?apikey=7c371e1e&s=blade+runner")
+    searched = document.getElementById("search-bar-input").value
+    
+    fetch(`https://www.omdbapi.com/?apikey=7c371e1e&s=${encodeURIComponent(searched)}&plot=short`)
     .then(res => res.json())
     .then(data => renderMovieHTML(data.Search)
     )    
+    
 }) 
 
-function getMovieHTML(movies){
+    
+    
+    
+
+// searched.forEach(movieDetails => 
+//     fetch(`https://www.omdbapi.com/?apikey=7c371e1e&s=${encodeURIComponent(movieDetails)}&plot=short`)
+// )
+
+
+
+function renderMovieHTML(data){
+        let myMovieHTML = ""
+        
+        data.forEach(movie => {
+        const movieId = movie.imdbID
+        fetch(`https://www.omdbapi.com/?apikey=7c371e1e&i=${movieId}&plot=short`)
+        .then(res => res.json())
+        .then(detailsData => { 
+            console.log(detailsData)
+        
+        myMovieHTML += getMovieHTML(detailsData)
+      document.getElementById("main").innerHTML =  myMovieHTML
+        }
+    )
+        // console.log(movieId)
+    })
+    //   document.getElementById("noDataEl").style.display = "none"
+}
+
+function getMovieHTML(data){
     let movieHTML = ""
-      
-    movies.forEach(movie => {
+    // console.log(data)
               movieHTML += `
         <section class="movie-section">
             <div class="poster-container">
-                <img class="movieImg" src="${movie.Poster}"/>    
+                <img class="movieImg" src="${data.Poster}"/>    
             </div>
             <div class="title-container-wrapper">
                 <div class="title-container">
-                    <h4 class="movie-title">${movie.Title}</h4>
+                    <h4 class="movie-title">${data.Title}</h4>
                     <ion-icon class="star" name="star"></ion-icon>
-                    <p class="rating">23</p>
+                    <p class="rating">${data.imdbRating}</p>
                 </div>
                 <div class="movie-details-container">
-                    <p class="movie-length">140min</p>
-                    <p class="movie-genre">Action, Comedy</p>
+                    <p class="movie-length">${data.Runtime}</p>
+                    <p class="movie-genre">${data.Genre}</p>
                     <div class="watchlist-button-container">
                         <ion-icon class="addBtn" name="add-circle"></ion-icon>
                         <p>Watchlist</p>
                     </div>
                 </div>
                 <div>
-                    <p>A Bladerunner must pursue and terminate four replicants who stole a ship in space, and have returned to earth to find their creator</p>
+                    <p>${data.Plot}</p>
                 </div>
             </div>
         </section>
         <hr style="border:0.5px solid lightgrey"/>`
-      })
+        
+      
       
       return movieHTML
+    // console.log(data)
 }
   
-function renderMovieHTML(data){
-      document.getElementById("main").innerHTML = getMovieHTML(data)
-}
+
+
+
 
